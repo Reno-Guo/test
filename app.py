002e4d28e1f45c88b038a14b68d406502b35b08f
Reno-Guo <reno.guo@oceanwing.com>
2025-07-25 15:37:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -316,7 +316,7 @@ def search_insight_viz_app():
     with st.expander("搜索流量洞察可视化", expanded=False):
         st.header("搜索流量洞察可视化")
 
-        uploaded_file = st.file_uploader("选择包含源数据的 Excel 文件", type=["xlsx", "xls"], key="viz_data_file")
+        uploaded_file = st.file_uploader("选择包含源数据的 Excel 文件(完成检查确认无误)", type=["xlsx", "xls"], key="viz_data_file")
         save_filename = st.text_input("请输入输出文件名（例如：viz_result.xlsx）", key="viz_save_folder")
 
         if st.button("执行可视化", key="viz_execute_button"):
